16091f034acee3b57e43ffcacddca65870b240a4
Tarun <ts371714@gmail.com>
2023-03-19 22:50:32 +0530

    first

---
+++ swaptwonumberassig.java
@@ -0,0 +1,16 @@
+public class swaptwonumberassig {
+    public static void main(String[] args) {
+        int numA = 63;
+        int numB = 15;
+        System.out.println("numA=" + numA);
+        System.out.println("numB=" + numB);
+        System.out.println("Swap the numbers");
+        int numC;
+        // sw a p the numbers
+        numC = numA;
+        numA = numB;
+        numB = numC;
+        System.out.println("numA=" + numA);
+        System.out.println("numB=" + numB);
+    }
+}
