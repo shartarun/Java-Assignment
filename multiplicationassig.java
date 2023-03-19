8ad5ae3d331d5e382bb27206b2947d72fcf647c5
Tarun <ts371714@gmail.com>
2023-03-19 22:51:56 +0530

    first

---
+++ multiplicationassig.java
@@ -0,0 +1,11 @@
+public class multiplicationassig {
+    public static void main(String[] args) {
+        int i;
+        int num = 5;
+        // take input values i and num
+        for (i = 1; i <= 10; i++) {
+            // using concatenate show 5*i=value as output
+            System.out.println("5*" + i + "=" + (num * i));
+        }
+    }
+}
