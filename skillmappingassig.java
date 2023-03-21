35ce5d49b92068db369279da233f227ce10ae151
Tarun <ts371714@gmail.com>
2023-03-22 00:03:50 +0530

    first

---
+++ skillmappingassig.java
@@ -0,0 +1,23 @@
+public class skillmappingassig {
+    public static void main(String[] args) {
+        int i, k, l;
+        int j;
+        for (k = 5; k >= 1; k--) {
+
+            for (l = 0; l <= k; l++) {
+                System.out.print("x");
+            }
+            System.out.println();
+
+        }
+        for (i = 0; i < 5; i++) {
+
+            for (j = 0; j <= i; j++) {
+                System.out.print("x");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
